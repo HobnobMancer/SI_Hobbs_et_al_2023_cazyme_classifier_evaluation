96009b8401a4423ae8b9cea754be74af33d5c268
HobnobMancer <eemh1@st-andrews.ac.uk>
2023-08-22 08:34:39 +0000

    add bash scripts to get cazy data

---
+++ scripts/cazy/get_cazy_sampling.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# (c) University of St Andrews 2023
+# (c) University of Strathclyde 2023
+# (c) James Hutton Institute 2023
+#
+# Author:
+# Emma E. M. Hobbs
+#
+# Contact
+# eemh1@st-andrews.ac.uk
+#
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# download_cazy
+
+# Get the number of unique GenBank protein IDs associated with each species in the local CAZy db
+
+sqlite3 data/cazy/cazy-db_2023-07-26.db -header -csv "
+SELECT K.kingdom, T.genus, T.species, COUNT(DISTINCT G.genbank_accession) AS CAZyme_count
+FROM Kingdoms AS K
+INNER JOIN Taxs AS T ON K.kingdom_id = T.kingdom_id
+INNER JOIN Genbanks AS G ON T.taxonomy_id = G.taxonomy_id
+GROUP BY T.species
+" > data/test_sets/cazy_species_sizes.csv
